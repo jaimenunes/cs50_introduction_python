29962f41ca8d523f270cf7149b20e326676a70e6
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2023-10-03 17:32:44 -0300

    wip lecture 2

---
+++ lecture_2/cat.py
@@ -0,0 +1,19 @@
+# print("Meow")
+# print("Meow")
+# print("Meow")
+
+# count = 3
+# while count !=0:
+#     print("Meow")
+#     count -= 1
+
+# for i in range(3):
+#     print("Meow")
+
+while True:
+    n = int(input("what's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("Meow")
