44edd2699299ab304ef79722b75cdc6f346fae85
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2023-10-06 09:24:06 -0300

    done lecture_2

--- lecture_2/hogwarts.py
+++ lecture_2/hogwarts.py
@@ -7,12 +7,38 @@
 # for i in range(len(students)):
 #     print(students[i])
 
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor", 
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"
-}
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor", 
+#     "Ron": "Gryffindor",
+#     "Draco": "Slytherin"
+# }
+
+# for student in students:
+    # print(f'{student} --> {students[student]}')
+
+students = [
+    {
+        "name": "Hermione",
+        "house": "Gryffindor", 
+        "patronus": "Otter"
+    },
+    {
+        "name": "Harry",
+        "house": "Gryffindor", 
+        "patronus": "Stag"
+    },
+    {
+        "name": "Ron",
+        "house": "Gryffindor", 
+        "patronus": "Jack Russell Terrier"
+    },
+    {
+        "name": "Hermione",
+        "house": "Slytherin", 
+        "patronus": None
+    },
+]
 
 for student in students:
-    print(f'{student} --> {students[student]}')
+    print(student['name'], student['house'], student['patronus'], sep=", ")
---
+++ lecture_2/mario.py
@@ -0,0 +1,18 @@
+def main():
+#      print_column(3)
+
+# def print_column(n):
+#     for _ in range(n):
+#         print("#")
+    print_square(3)
+
+def print_square(size):
+    # for each brick in row
+    for i in range(size):
+        print ("#" * size)
+        # Print each brick
+        # for j in range(size):
+        #     print("#", end="")
+        # print()
+
+main()
