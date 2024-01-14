c01a03d6fc71da67a715912d4ba6a9e2db30f8f2
jaime nunes <jaimenunesleal@gmail.com>
2024-01-14 17:19:20 -0300

    Lecture 04

--- lecture_3/number.py
+++ lecture_3/number.py
@@ -1,6 +1,10 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("It's not integer")
-
-print(f"x is {x}")
+def main():
+    x = get_int("What the x? ")
+    print(f'X is {x}')
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+main()
