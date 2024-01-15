6f9f65e69381fee2d075225e8e51910977d1c46a
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2024-01-15 08:36:41 -0300

    update lecture 4

--- lecture_4/library.py
+++ lecture_4/library.py
@@ -1,3 +1,15 @@
+import random
+
+# coin = random.choice(['heads', 'tails'])
+# print(coin)
+
+# number = random.randint(1, 10)
+# print(number)
+
+# cards = ['jack', 'queen', 'king']
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
 import statistics
 
 grade = statistics.mean([100, 90])
